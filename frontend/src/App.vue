07e4e9d94dee9f9dea4c50155df04b5e78aea9b6
Amit P <aspper20@gmail.com>
2022-02-27 21:06:35 +0530

    feat: added frontend boilerplate code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="bg-blue-50">
+    <loading />
+    <default-toast />
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapActions, mapGetters } from 'vuex';
+import Loading from './components/common/loading.vue';
+import DefaultToast from './components/toasts/default-toast.vue';
+import * as authTypes from './store/modules/auth/auth-types';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DefaultToast,
+    Loading,
+  },
+  computed: {
+    ...mapGetters({
+      profileData: authTypes.GET_PROFILE_DATA,
+    }),
+  },
+  methods: {
+    ...mapActions({
+      getProfileData: authTypes.GET_PROFILE_DATA_ACTION,
+    }),
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
